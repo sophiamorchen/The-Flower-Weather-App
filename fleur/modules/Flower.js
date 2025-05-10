40890a41ff95225042d060a41ab718f204cafc6f
Sophia M <sophia.moerchen@gmail.com>
2025-05-10 14:18:16 +0200

    Ajout de la fleur (terminée)

--- fleur/modules/Flower.js
+++ fleur/modules/Flower.js
@@ -1,9 +1,139 @@
-
-
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 class Flower {
     constructor() {
-        console.log("Flower module loaded");
+        // Étape 1 : Initialisation des propriétés de la classe / définir les variables
+        // on défini la heuteur et la largeur du canvas
+        this.canvas = document.querySelector(".js-canvas")
+        this.canvasWidth = window.innerWidth
+        this.canvasHeight = window.innerHeight
+        this.animate = this.animate.bind(this)
+        this.flowerSize = 50
+
+        this.init()
     }
+// fonction qui lance les fonctions d'instanciation de ma classe
+// c'est la fonction qui va être lancée automatiquement
+    init () {
+        this.createScene()
+        this.createCamera()
+        this.createRender()
+
+        this.createGroupOfObjects()
+        this.createStem()
+        this.createPistil()
+        this.createPetals()
+        
+        this.createOrbitControls()
+        this.createHelper()
+        
+        this.addGroupToScene()
+
+        this.animate()
+
+    }
+    createScene() {
+        this.scene = new THREE.Scene()
+        this.scene.background = new THREE.Color('skyblue')
+    }
+    createCamera() {
+        const aspectRatio = this.canvasWidth / this.canvasHeight
+        this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+        this.camera.position.z = 100
+        this.camera.position.y = 80
+        this.camera.position.x = 0
+    }
+    createOrbitControls() {
+        this.controls = new OrbitControls( this.camera, this.renderer.domElement );  
+    }
+    createHelper() {
+        const axesHelper = new THREE.AxesHelper( 50 )
+        this.scene.add( axesHelper )
+
+        const helper = new THREE.CameraHelper( this.camera );
+        this.scene.add( helper );
+
+        // const spotLight = new THREE.SpotLight( 0xffffff );
+        // spotLight.position.set( 10, 10, 10 );
+        // this.scene.add( spotLight );
+
+        // const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+        // this.scene.add( spotLightHelper );
+
+        // const light = new THREE.DirectionalLight( 0xFFFFFF );
+        // this.scene.add( light );
+
+        // const helper1 = new THREE.DirectionalLightHelper( light, 5 );
+        // this.scene.add( helper1 );
+    }
+    createRender() {
+        this.renderer = new THREE.WebGLRenderer()
+        this.renderer.setSize(this.canvasWidth, this.canvasHeight)
+        this.canvas.appendChild(this.renderer.domElement)
+    }
+    // on ajoute le gorupejuste avant de mettre la fleur
+    createGroupOfObjects() {
+        this.flowerGroup = new THREE.Group()
+
+    }
+    createStem() {
+        const geometry = new THREE.CylinderGeometry( 1, 1, this.flowerSize, 32 )
+        const color = new THREE.Color('rgb(0, 255, 0)')
+        const material = new THREE.MeshBasicMaterial( {color: color} )
+        const cylinder = new THREE.Mesh( geometry, material )
+        this.flowerGroup.add(cylinder)
+    }
+    createPistil() {
+        const geometry = new THREE.SphereGeometry( 4, 32, 16 ); 
+        const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
+        const sphere = new THREE.Mesh( geometry, material )
+        sphere.position.y = this.flowerSize/2
+        this.flowerGroup.add( sphere )
+    }
+    createPetals() { 
+        const color = new THREE.Color('rgb(255, 0, 0)')
+        const geometry = new THREE.TorusGeometry( 8, 0.5, 16, 100 ); 
+        const material = new THREE.MeshBasicMaterial( { color: color } ); 
+        const positionX = [-5, 0, 5, 0]
+        const positionZ = [0, -5, 0, 5]
+        const rotationY = [-30, 0, 30, 0]
+        const rotationX = [90, 120, 90, 60]
+        for (let i = 0 ; i < 4 ; i++) {
+            const torus = new THREE.Mesh( geometry, material ); 
+            torus.position.y = this.flowerSize/2
+            torus.position.x = positionX[i]
+            torus.position.z = positionZ[i]
+            /*
+    🔁 Ce que font tes rotations :
+
+    rotation.x : incline le tore vers l’avant ou l’arrière.
+
+    rotation.y : fait tourner autour du centre (comme tourner une assiette sur elle-même).
+
+    rotation.z : ferait pencher sur le côté (gauche/droite).
+            */
+            torus.rotation.x = THREE.MathUtils.degToRad(rotationX[i]) // feuille avant ou arriere
+            torus.rotation.y = THREE.MathUtils.degToRad(rotationY[i]) // feuille gauche et droite.
+
+            this.flowerGroup.add( torus );
+        }
+
+    }
+    addGroupToScene() {
+        this.scene.add(this.flowerGroup)
+    }
+
+    animate() {
+        /* animation de la fleur : 
+        - [] si pas de ville seldctionnée, la fleur tourne sur elle même
+        - [] si une ville est sélectionnée, la fleur s'oriente en fonction des donénes API
+        */
+        requestAnimationFrame(this.animate)
+        // this.flowerGroup.rotation.y += 0.01
+        this.controls.update()
+        this.renderer.render(this.scene, this.camera)
+    }
+
 }
 
 export {Flower}
